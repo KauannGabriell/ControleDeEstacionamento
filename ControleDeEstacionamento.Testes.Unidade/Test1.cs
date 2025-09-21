94f53466b170da6d1a790330d4e19fce667b64d7
coelho-glith <coelho.soldado7@gmail.com>
2025-09-21 17:15:40 -0300

    Criado a pasta de Testes para o projeto.

---
+++ ControleDeEstacionamento.Testes.Unidade/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeEstacionamento.Testes.Unidade
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
